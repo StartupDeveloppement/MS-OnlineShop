51bb2eaa469d89ad9f1267252496e86c2f3cafaf
Beulaygue <guillaume.beulaygue@ynov.com>
2016-01-13 19:19:54 +0100

    bla

---
+++ IShop/IShop/Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IShop.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Connexion(UTILISATEUR utilisateur)
+        {
+            bool result;
+            Models.AccountModels accountModels = new Models.AccountModels();
+            result = accountModels.Login(utilisateur.EMAIL_UTILISATEUR, utilisateur.MDP_UTILISATEUR);
+            if (result)
+            {
+                return View("Register");
+            }
+            else
+            {
+                return View("Login");
+            }
+        }
+    }
+}
---
+++ IShop/IShop/Models/AccountModels.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IShop.Models
+{
+    public class AccountModels
+    {
+        public UTILISATEUR utilisateur;
+        bool result;
+
+        public AccountModels()
+        {
+
+        }
+
+        public bool Login(string email, string mdp)
+        {
+            using (var db = new DataBaseIShopEntities())
+            {
+                utilisateur = db.UTILISATEUR.Where(u => u.EMAIL_UTILISATEUR == email)
+                    .Where(u => u.MDP_UTILISATEUR == mdp).First();
+                if (utilisateur != null)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
