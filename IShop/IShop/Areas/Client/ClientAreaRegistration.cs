bc202b21457fb825c840a3344762aa80c0b3564e
Beulaygue <guillaume.beulaygue@ynov.com>
2016-04-13 16:32:41 +0200

    session

---
+++ IShop/IShop/Areas/Client/ClientAreaRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace IShop.Areas.Client
+{
+    public class ClientAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Client";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Client_default",
+                "Client/{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "IShop.Controllers" }
+                //new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
